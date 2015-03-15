aa675c690dd5b115289142cf67a94acb8c519de8
pig0826usanj <pig0826usanj@gmail.com>
2015-03-15 15:19:25 -0700

    changed port to 5000

--- hello.js
+++ hello.js
@@ -8,5 +8,5 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(5000);
 console.log("success");
